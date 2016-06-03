21ccba4a942a3995b4b9c7b65f49f30d9482d453
Ryuuven <abarbin.ir2018@esaip.org>
2016-06-03 14:56:26 +0200

    Dossier Antoine
    Dossier, avec le debut du header / menu (codé salement, je dois faire un
    autre fichier css + l'indentation et les commentaires

---
+++ entredeuxAntoine/view/header.php
@@ -0,0 +1,133 @@
+<html>
+    <head>
+        <title>Fest'Esaip</title>
+        <style>
+            
+            body {
+                background-image: url('wallpaper.png');
+            }   
+            
+            div {
+                text-align : center;
+                background-color: #000000;
+                opacity: 0.70;
+                filter: alpha(opacity=70);
+            }
+            
+            div a {
+                text-decoration: none;
+                color: white;
+                font-size: 20px;
+                padding: 15px ;
+                display:inline-block;
+            }
+            
+            ul {
+              display: inline;
+              margin: 0;
+              padding: 0;
+            }
+            
+            ul li {
+                display: inline-block;
+            }
+            
+            ul li:hover {
+                background: goldenrod;
+                
+            }
+            
+            
+            ul li:hover ul {
+                display: block;
+            }
+            
+            ul li ul {
+              position: absolute;
+              width: 200px;
+              display: none;
+            }
+            
+            ul li ul li { 
+              background: #555; 
+              display: block; 
+            }
+            
+            ul li ul li a {
+                display:block !important;
+            } 
+            ul li ul li:hover {
+                background: #666;
+            }
+        </style>
+        
+    </head>
+    <body>
+        <header>
+            <div>
+                <ul>
+                    <li>
+                        <a href="">Accueil</a>						
+                    </li>
+
+                    <li>
+                        <a href="">Fest'Esaip</a>
+                            <ul>
+                                <li>
+                                    <a href="">Historique</a>
+                                </li>
+                                <li>
+                                    <a href="">Équipes / Contact</a>
+                                </li>
+                                <li>
+                                    <a href="">Sponsors</a>
+                                </li>
+                            </ul>
+                    </li>				
+                    <li>
+                        <a href="">Programmation</a>
+                            <ul>
+                                                            <li>
+                                                                    <a href="">Animations</a>
+                                                            </li>
+                                                            <li>
+                                                                    <a href="">Stands</a>
+                                                            </li>
+                                                            <li>
+                                                                    <a href="">Concerts</a>
+                                                            </li>
+                                                    </ul>
+                                    </li>
+
+                                    <li>
+                                            <a href="">Infos pratiques</a>
+                                                    <ul>
+                                                            <li>
+                                                                    <a href="">Camping</a>
+                                                            </li>
+                                                            <li>
+                                                                    <a href="">Accessbilité</a>
+                                                            </li>
+                                                            <li>
+                                                                    <a href="">Venir au Fest'Esaip</a>
+                                                            </li>
+                                                    </ul>
+                                    </li>
+
+                                    <li>
+                                            <a href="">S'inscrire / Tickets</a>
+                                    </li>
+
+                                    <li>
+                                            <a href="">zone de co/deco</a>
+                                    </li>
+                            </ul>
+            </div>
+
+        </header>
+    </body>
+</html>
+
+
+
+
